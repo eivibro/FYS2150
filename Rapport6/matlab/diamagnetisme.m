6c257096b357819ab0fe4bf6d33ded33a7be8cec
Eivind Brox <eivbro@gmail.com>
2016-06-03 17:36:54 +0200

    Snart ferdig med resultater!

--- Rapport6/matlab/diamagnetisme.m
+++ Rapport6/matlab/diamagnetisme.m
@@ -16,6 +16,7 @@ g = 9.82;
 B1_squared = (measurements(:,3)*1e-3).^2;
 vekt = measurements(:,1);
 kraft = g*(max(vekt)-vekt)*1e-3;
+%kraft = g*vekt*1e-3;
 
 [m, c, dm, dc] = uncertaintyLinearFitting(B1_squared...
     (1:length(B1_squared)), kraft(1:length(kraft)));
@@ -30,12 +31,40 @@ xlabel('B_1^2 [T^2]')
 legend('Målinger', 'Lineær tilpasning', 'Location', 'northwest')
 set(gca, 'FontSize', 16)
 
-saveas(gcf, 'tilpasning', 'epsc')
+%saveas(gcf, 'tilpasning', 'epsc')
 
 %Estimating area of cross section for the vismut rod
 A = pi*(mean_D*1e-3)^2/4;
 permeability = 4*pi*1e-7;
 
 chi = -2*permeability*m/A;
-dChi = chi*sqrt((dm/m)^2+(2*standard_error_diameter/mean_D)^2+0.1^2); 
-chi-dChi
+dm = 0;
+dChi = chi*sqrt((dm/m)^2+(2*standard_error_diameter/mean_D)^2+(2*0.05)^2); 
+
+B1 = measurements(:,3)*1e-3;
+[m2, c2, dm2, dc2] = uncertaintyLinearFitting(B1, kraft);
+
+Force_lin2 = @(B) c2+m2*B;
+figure(2)
+plot(B1, kraft, '-ob')
+hold on
+plot(B1, Force_lin2(B1), 'r')
+chi2 = -2*permeability*m2/A;
+
+%Relativ forskjell mellom målte og lineært tilpassede verdier
+relativ1 = (kraft-Force_lin(B1_squared))./kraft;
+relativ2 = (kraft-Force_lin2(B1))./kraft;
+figure(3)
+plot(B1, relativ1*100, '-b')
+hold on
+plot(B1, relativ2*100, '--r')
+set(gca, 'FontSize', 16)
+xlabel('Magnetfeltmålinger [T]')
+ylabel('Relativ forskjell i %')
+legend('F_z og B_1^2', 'F_z og B_1', 'Location', 'southeast')
+
+%saveas(gcf, 'relativForskjell', 'epsc')
+rms1 = sum(((kraft-Force_lin(B1_squared))-...
+    mean(kraft-Force_lin(B1_squared))).^2)/length(kraft);
+rms2 = sum(((kraft-Force_lin2(B1))-...
+    mean(kraft-Force_lin2(B1))).^2)/length(kraft);
